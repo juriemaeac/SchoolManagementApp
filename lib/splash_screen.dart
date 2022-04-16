faf385b1026fdef78cc437c44d624f7b2701ec3f
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2022-04-17 02:16:36 +0800

    login ui w/ splash

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -31,6 +31,8 @@ class _SplashScreenState extends State<SplashScreen>
           setState(() {});
         });
       }
+
+
     });
   }
 
@@ -44,12 +46,20 @@ class _SplashScreenState extends State<SplashScreen>
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.orange,
+      backgroundColor: Colors.white,
       
       body: Stack(
-        
         children: [
+          Visibility(visible: copAnimated, child: Row(
+              children: [
+                LoginScreen(), //ung nasa left, lalabas kapag maliit
+                if (MediaQuery.of(context).size.width > 900)
+                  LoginScreen(), //login mismo
+              ],
+            ),
+          ),
           // White Container top half
+          if (MediaQuery.of(context).size.width > 900)
           AnimatedContainer(
             duration: const Duration(seconds: 1),
             width: copAnimated ? screenWidth/2 : screenWidth,
@@ -73,17 +83,13 @@ class _SplashScreenState extends State<SplashScreen>
                         ..duration = composition.duration
                         ..forward();
                     },
-                    
                   ),
                 ),
-                // Visibility(
-                //   visible: copAnimated,
-                //   child: Image.asset(
-                //     'assets/education.png',
-                //     height: 150.0,
-                //     width: 150.0,
-                //   ),
-                // ),
+                
+                Visibility(
+                  visible: copAnimated ,
+                  child: const LoginPageRightSide(),
+                ),
                 // Center(
                 //   child: AnimatedOpacity(
                 //     opacity: animateCafeText ? 1 : 0,
@@ -98,15 +104,7 @@ class _SplashScreenState extends State<SplashScreen>
             ),
           ),
 
-          // Text bottom part
-          Visibility(visible: copAnimated, child: Row(
-              children: [
-                LoginScreen(),
-                if (MediaQuery.of(context).size.width > 900)
-                  const LoginPageRightSide(),
-              ],
-            ),
-          ),
+          
         ],
       ),
     );
@@ -115,59 +113,3 @@ class _SplashScreenState extends State<SplashScreen>
 
 
 
-// class _BottomPart extends StatelessWidget {
-//   const _BottomPart({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Align(
-//       alignment: Alignment.center,
-      
-//       child: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 40.0),
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           crossAxisAlignment: CrossAxisAlignment.end,
-//           mainAxisAlignment: MainAxisAlignment.end,
-//           children: [
-//             const Text(
-//               'Find The Best Coffee for You',
-//               style: TextStyle(
-//                   fontSize: 27.0,
-//                   fontWeight: FontWeight.bold,
-//                   color: Colors.white),
-//             ),
-//             const SizedBox(height: 30.0),
-//             Text(
-//               'Lorem ipsum dolor sit amet, adipiscing elit. '
-//               'Nullam pulvinar dolor sed enim eleifend efficitur.',
-//               style: TextStyle(
-//                 fontSize: 15.0,
-//                 color: Colors.white.withOpacity(0.8),
-//                 height: 1.5,
-//               ),
-//             ),
-//             const SizedBox(height: 50.0),
-//             Align(
-//               alignment: Alignment.centerRight,
-//               child: Container(
-//                 height: 85.0,
-//                 width: 85.0,
-//                 decoration: BoxDecoration(
-//                   shape: BoxShape.circle,
-//                   border: Border.all(color: Colors.white, width: 2.0),
-//                 ),
-//                 child: const Icon(
-//                   Icons.chevron_right,
-//                   size: 50.0,
-//                   color: Colors.white,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 50.0),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
