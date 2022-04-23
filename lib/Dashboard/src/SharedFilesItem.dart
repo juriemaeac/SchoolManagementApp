947ed777dd88a10c386612fbfb69c29c5382a749
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2022-04-24 05:10:00 +0800

    dashboard ui

--- lib/Dashboard/src/SharedFilesItem.dart
+++ lib/Dashboard/src/SharedFilesItem.dart
@@ -4,16 +4,16 @@ import 'package:google_fonts/google_fonts.dart';
 class SharedFilesItem extends StatefulWidget {
   final String sharedFileName;
   final Color color;
-  final String members;
-  final String et;
-  final String fileSize;
+  final String first;
+  final String second;
+  final String third;
   final IconData icon;
 
   SharedFilesItem({
     required this.color,
-    required this.et,
-    required this.fileSize,
-    required this.members,
+    required this.second,
+    required this.third,
+    required this.first,
     required this.sharedFileName,
     required this.icon,
   });
@@ -54,81 +54,108 @@ class _SharedFilesItemState extends State<SharedFilesItem> {
                   ]
                 : []),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Container(
+              width: MediaQuery.of(context).size.width * 0.63,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 15.0,
-                      ),
-                      Container(
-                        height: 28.0,
-                        width: 28.0,
-                        decoration: BoxDecoration(
-                          color: widget.color.withOpacity(0.2),
-                          borderRadius: BorderRadius.circular(5.0),
+                  Container(
+                    width: 180,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 15.0,
                         ),
-                        child: Center(
-                          child: Icon(
-                            widget.icon,
-                            color: widget.color,
-                            size: 15.0,
+                        Container(
+                          height: 28.0,
+                          width: 28.0,
+                          decoration: BoxDecoration(
+                            color: widget.color.withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          child: Center(
+                            child: Icon(
+                              widget.icon,
+                              color: widget.color,
+                              size: 15.0,
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 15.0,
-                      ),
-                      Text(
-                        widget.sharedFileName,
-                        style: GoogleFonts.quicksand(
-                          color: hovered? widget.color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12.0,
+                        SizedBox(
+                          width: 15.0,
                         ),
-                      ),
-                    ],
+                        Text(
+                          widget.sharedFileName,
+                          style: GoogleFonts.quicksand(
+                            color: hovered? widget.color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12.0,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30.0),
-                        child: Text(
-                          widget.members,
-                          style: GoogleFonts.quicksand(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 11.0,
-                            color: Colors.black45,
-                          ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 130,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal:8.0),
+                                child: Text(
+                                  widget.first,
+                                  textAlign: TextAlign.left,
+                                  style: GoogleFonts.quicksand(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 11.0,
+                                    color: Colors.black45,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: 235,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text(
+                                  widget.second,
+                                  textAlign: TextAlign.left,
+                                  style: GoogleFonts.quicksand(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 11.0,
+                                    color: Colors.black45,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 30.0),
+                                child: Text(
+                                  widget.third,
+                                  textAlign: TextAlign.left,
+                                  style: GoogleFonts.quicksand(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 11.0,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30.0),
-                        child: Text(
-                          widget.et,
-                          style: GoogleFonts.quicksand(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 11.0,
-                            color: Colors.black45,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 30.0),
-                        child: Text(
-                          widget.fileSize,
-                          style: GoogleFonts.quicksand(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 11.0,
-                            color: Colors.black87,
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ],
               ),
